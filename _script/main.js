69270ff212803a447597965e6e8790693da82141
Yuka Suzuki <yukasuzuki@YukanoMacBook-Air.local>
2024-01-09 20:28:38 +0900

    カルーセルのアクセシビリティ対応完了

--- _script/main.js
+++ _script/main.js
@@ -2,13 +2,17 @@ $(function(){
   $("#slider").slick({
       slidesToShow: 3,
       // appendDots: $('.dots-2'),
-	    autoplay: true,         //自動再生
+	    autoplay: false,         //自動再生
 	    autoplaySpeed: 2000,    //自動再生のスピード
 	    speed: 800,	            //スライドするスピード
 	    dots: true,             //スライドしたのドット
 	    arrows: true,           //左右の矢印
 	    infinite: true,         //スライドのループ
   });
+
+  // 要修正:スライドにtabフォーカスを当てない。しかし矢印でカルーセルを回すと動的にtabindex=0になるので当たってしまう
+  // スライドにフォーカス当たっていいならこれいらない
+  // $('#slider .slick-slide').attr('tabindex', '-1')
 });
 
 
@@ -20,5 +24,6 @@ $(function() {
     dots: true,
     centerMode: true,
     centerPadding: '0px',
+    accessibility: true,
   });
 });
